8b8f691616b0c800cf29507cd4302d7dc8756bbe
Vlad <ksand.1@ya.ru>
2020-10-01 01:39:18 +0400

    build template store

---
+++ precache-manifest.6fc7b90642f0a63513dd7bf2e41364c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775d4d4e2155b0231a8a9dc5ed6bfca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b20d34e7b6918f2f99f",
+    "url": "/static/css/main.9f76eaf3.chunk.css"
+  },
+  {
+    "revision": "77d36173e0da66a39554",
+    "url": "/static/js/2.60eeaaa9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.60eeaaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b20d34e7b6918f2f99f",
+    "url": "/static/js/main.5eb238a6.chunk.js"
+  },
+  {
+    "revision": "89cf5b450cd5ef2488f8",
+    "url": "/static/js/runtime-main.07a51302.js"
+  }
+]);
